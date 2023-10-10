ba412a54c4713f72dcd367f5f60811af3a85844c
JOICE0409 <147546201+JOICE0409@users.noreply.github.com>
2023-10-10 18:48:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.getElementById("SAIBA COMO").addEventListener("click", function() {
+    alert("Botão 1 foi clicado!");
+});
+
+document.getElementById("CONTATO").addEventListener("click", function() {
+    alert("Botão 2 foi clicado!");
+
+    document.getElementById("botao3").addEventListener("click", function() {
+        alert("Botão 3 foi clicado!");
+
+});
